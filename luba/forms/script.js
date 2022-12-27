2fbd725952cf2a5656c985bf3b5c1300c3451b6d
lubamark <lubamark@mail.ru>
2022-12-27 21:41:47 +0400

    + работа с формами

--- luba/forms/script.js
+++ luba/forms/script.js
@@ -4,34 +4,40 @@ console.log('forms!');
 
 window.onload = function () {
   //текстареа
-  // const textarea = document.getElementById('textarea');
-  // const blockT = document.getElementById('blockTextarea');
-  // const unblockT = document.getElementById('unblockTextarea');
-  // const readonlyT = document.getElementById('readonlyTextarea');
-  // const editableT = document.getElementById('editableTextarea');
-  // console.log(textarea.innerHTML);
-  // console.log(textarea.textContent);
-  // console.log(textarea.value);
-  // textarea.value = 'Hello textarea!';
-  // console.log(textarea.value);
+  const textarea = document.getElementById('textarea');
+  const blockT = document.getElementById('blockTextarea');
+  const unblockT = document.getElementById('unblockTextarea');
+  const readonlyT = document.getElementById('readonlyTextarea');
+  const editableT = document.getElementById('editableTextarea');
+  console.log(textarea.innerHTML);
+  console.log(textarea.textContent);
+  console.log(textarea.value);
+  textarea.value = 'Hello textarea!';
+  console.log(textarea.value);
 
-  // blockT.addEventListener('click', function disableElement() {
-  //   textarea.disabled = true;
-  //   console.log(textarea.value);
-  // });
+  blockT.addEventListener('click', function disableElement() {
+    textarea.disabled = true;
+    console.log(textarea.value);
+  });
 
-  // unblockT.addEventListener('click', function enableElement() {
-  //   textarea.disabled = false;
-  // });
+  unblockT.addEventListener('click', function enableElement() {
+    textarea.disabled = false;
+  });
 
-  // readonlyT.addEventListener('click', function () {
-  //   textarea.readOnly = true;
-  //   console.log(textarea.value);
-  // })
+  readonlyT.addEventListener('click', function () {
+    textarea.readOnly = true;
+    console.log(textarea.value);
+  })
 
-  // editableT.addEventListener('click', function () {
-  //   textarea.readOnly = false;
-  // })
+  editableT.addEventListener('click', function () {
+    textarea.readOnly = false;
+  })
+
+  textarea.addEventListener('input', function () {
+    console.log(this.value);
+  })
+
+
 
   //инпут
   const inputNumber = document.getElementById('inputNumber');
@@ -49,7 +55,7 @@ window.onload = function () {
   })
 
   // переключение аттрибута
-  const textarea = document.getElementById('textarea');
+
   const toggleButton = document.getElementById('toggleDisabled');
 
   toggleButton.addEventListener('click', function () {
@@ -67,5 +73,21 @@ window.onload = function () {
       }
     }
   })
+
+  // select
+  const select = document.getElementById('select1');
+  const options = select.getElementsByTagName('option');
+  const selectButton = document.getElementById('changeSelectValue');
+
+  select.addEventListener('change', function () {
+    for (let i = options.length - 1; i >= 0; i--) {
+      options[i].disabled = !options[i].disabled;
+    }
+  });
+
+  selectButton.addEventListener('click', function () {
+    select.value = 'banana'; // если нет value, то пишем текст в тэгах
+  })
+
 
 }
